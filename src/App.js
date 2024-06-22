f069d2b0e6d076124f3d6cf96262ecea849dbe0a
Ayush Sharma <ayush22@navgurukul.org>
2024-06-23 02:46:52 +0530

    Consult-WebPage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import UpperNavbar from './Components/Navbar-Top/Navbar';
 import MainNavbar from './Components/Navbar/MainNavbar';
 import Employee_Profile from './Components/Employee-Image/Employee_Profile';
+import AboutConsult from './Components/AboutConsult/AboutConsult';
+import Offers from './Components/Offers/Offers';
 
 
 
@@ -12,6 +14,8 @@ function App() {
     <UpperNavbar/>
     <MainNavbar/>
     <Employee_Profile/>
+    <AboutConsult/>
+    <Offers/>
     </>
   );
 }
