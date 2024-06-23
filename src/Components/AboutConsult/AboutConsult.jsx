12bd778bbbb207a84a24cf485c9fda262125ad4c
Ayush Sharma <ayush22@navgurukul.org>
2024-06-23 23:33:44 +0530

    Consult WebPage

--- src/Components/AboutConsult/AboutConsult.jsx
+++ src/Components/AboutConsult/AboutConsult.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './AboutConsult.css';
 
-
 function AboutConsult() {
     return (
         <div className="first_about">
@@ -14,15 +13,15 @@ function AboutConsult() {
                     <h6 className='long-content'>
                         Et stet ipsum nonumy rebum eos justo, accusam invidunt aliquyam stet magna at et sanctus, vero sea sit amet dolores, sit dolor duo invidunt dolor, kasd rebum consetetur diam invidunt erat stet. Accusam nonumy lorem kasd eirmod. Justo clita sadipscing ea invidunt rebum sadipscing consetetur. Amet dlam amet amet sanctus sanctus invidunt erat ipsum eirmod.
                     </h6>
-                    <button style={{borderRadius : "50px"}}>
+                    <button style={{ borderRadius: "50px" }}>
                         Get a quote
                     </button>
                 </div>
             </div>
             <div className="about_">
                 <div className='profile_first'>
-                    <div className='image_profile'>
-                        <i className="fa-solid fa-user-tie" style={{ color: 'red', fontSize: "1.5rem" }} ></i>
+                    <div className='image_profile d-flex justify-content-center align-items-center p-3 p-md-0'>
+                        <i className="fa-solid fa-user-tie" style={{ color: 'red', fontSize: "1.5rem" }}></i>
                     </div>
                     <div className='profile_text'>
                         <h2 className='h2'>Business Planning</h2>
@@ -30,9 +29,8 @@ function AboutConsult() {
                     </div>
                 </div>
                 <div className='profile_first'>
-                    <div className='image_profile'>
+                    <div className='image_profile d-flex justify-content-center align-items-center p-3 p-md-0'>
                         <i className="fa-solid fa-chart-line" style={{ color: 'red', fontSize: "1.5rem" }}></i>
-
                     </div>
                     <div className='profile_text'>
                         <h2 className='h2'>Market Analysis</h2>
@@ -40,16 +38,16 @@ function AboutConsult() {
                     </div>
                 </div>
                 <div className='profile_first'>
-                    <div className='image_profile'>
+                    <div className='image_profile d-flex justify-content-center align-items-center p-3 p-md-0'>
                         <i className="fa-solid fa-scale-balanced" style={{ color: 'red', fontSize: "1.5rem" }}></i>
-
                     </div>
                     <div className='profile_text'>
-                        <h2 className='h2'>Legel Advisory</h2>
+                        <h2 className='h2'>Legal Advisory</h2>
                         <p className='para'>Tempor erat elitr rebum at clita. Díam dolor ipsum amet eos erat ipsum lorem et sit sed stet lorem sit clita duo</p>
                     </div>
                 </div>
             </div>
+
         </div>
     );
 }
