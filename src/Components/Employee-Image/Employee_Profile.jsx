dc41fca3d236147da22e142e3266da85daa7a117
Ayush Sharma <ayush22@navgurukul.org>
2024-06-23 23:35:47 +0530

    Consult WebPage

--- src/Components/Employee-Image/Employee_Profile.jsx
+++ src/Components/Employee-Image/Employee_Profile.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import './Employee.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function EmployeeProfile() {
     return (
-        <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary" id='main-container'>
-            <div className="col-md-6 p-lg-5 mx-auto my-5" id='main-text'>
-                <p className="display-12 fw-bold">BUSINESS CONSULTANCY</p>
-                <h1 className="display-6 fw-bold" id='EmployeeText'>We Provide Solutions On Your Business</h1>
-                <div className="d-flex gap-3 justify-content-center lead fw-normal">
-                    <button type="button" className="btn btn-outline-danger">Get Quote</button>
-                    <button type="button" className="btn btn-outline-danger">Buy</button>
+        <div>
+            <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary" id='main-Container'>
+                <div class="col-md-6 p-lg-5 mx-auto my-5" style={{color:'white'}}>
+                    <p>BUSINESS CONSULTANCY</p>
+                    <h2 class="display-5 fw-bold">We Provide Solution On</h2>
+                    <h2 class="display-3 fw-bold">Your Business</h2>
+                    <div class="d-flex gap-3 justify-content-center lead fw-normal">
+                        <button type="button" class="btn btn-danger">Get Quote</button>
+                        <button type="button" class="btn btn-primary">Contact Us</button>
+                    </div>
                 </div>
+
+                <div class="product-device shadow-sm d-none d-md-block"></div>
+                <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
             </div>
-            <div className="product-device shadow-sm d-none d-md-block"></div>
-            <div className="product-device product-device-2 shadow-sm d-none d-md-block"></div>
         </div>
-    );
+    )
 }
+
 
 export default EmployeeProfile;
