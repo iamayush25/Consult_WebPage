de8324fbc1a661283bbb6a2f179948cb10212871
Ayush Sharma <ayush22@navgurukul.org>
2024-06-23 23:36:18 +0530

    Consult WebPage

--- src/Components/Navbar/MainNavbar.jsx
+++ src/Components/Navbar/MainNavbar.jsx
@@ -4,24 +4,28 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function MainNavbar() {
     return (
-        <div>
-            <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-                <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <nav className="navbar navbar-expand-md navbar-light bg-light py-3 mb-4">
+            <div className="container">
+                <a href="/" className="navbar-brand d-flex align-items-center">
                     <svg className="bi me-2" width="40" height="32">
                         <use xlinkHref="#bootstrap" />
                     </svg>
                     <span className="fs-4">Consult</span>
                 </a>
-
-                <ul className="nav nav-pills">
-                    <li className="nav-item"><a href="#" className="nav-link" aria-current="page">Home</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link">About</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link">Service</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link">PAGES</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link">Contact</a></li>
-                </ul>
-            </header>
-        </div>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item"><a href="#" className="nav-link" aria-current="page">Home</a></li>
+                        <li className="nav-item"><a href="#" className="nav-link">About</a></li>
+                        <li className="nav-item"><a href="#" className="nav-link">Service</a></li>
+                        <li className="nav-item"><a href="#" className="nav-link">PAGES</a></li>
+                        <li className="nav-item"><a href="#" className="nav-link">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
 
