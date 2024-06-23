cb2cf7a4090048aa79028c1612c74180889d3b29
Ayush Sharma <ayush22@navgurukul.org>
2024-06-23 23:36:42 +0530

    Consult WebPage

--- src/Components/Navbar-Top/Navbar.jsx
+++ src/Components/Navbar-Top/Navbar.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import './Navbar-Top.css';
-// import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const UpperNavbar = () => {
     return (
-        <div className="Nav">
-            <div className="Nav-left">
-                <a href="#faqs">FAQs</a>
-                <hr className='hr'/>
-                <a href="#support">Support</a>
-                <hr className='hr'/>
-                <a href="#privacy">Privacy</a>
-                <hr className='hr'/>
-                <a href="#policy">Policy</a>
-                <hr className='hr'/>
-                <a href="#career">Career</a>
-            </div>
-            <span className="Nav-right">
-                <div className="contact-info">
-                    <p>contact@example.com</p>
+        <div className="container">
+            <div className="Nav">
+                <div className="Nav-left">
+                    <a href="#faqs" className="nav-link">FAQs</a>
                     <hr className='hr'/>
-                    <p>+1234567890</p>
+                    <a href="#support" className="nav-link">Support</a>
+                    <hr className='hr'/>
+                    <a href="#privacy" className="nav-link">Privacy</a>
+                    <hr className='hr'/>
+                    <a href="#policy" className="nav-link">Policy</a>
+                    <hr className='hr'/>
+                    <a href="#career" className="nav-link">Career</a>
                 </div>
-            </span>
+                <span className="Nav-right">
+                    <div className="contact-info">
+                        <p>contact@example.com</p>
+                        <hr className='hr'/>
+                        <p>+1234567890</p>
+                    </div>
+                </span>
+            </div>
         </div>
     );
 };
