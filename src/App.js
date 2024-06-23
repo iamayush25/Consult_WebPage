acabe77321c970edc787439821ad3288a382c08c
Ayush Sharma <ayush22@navgurukul.org>
2024-06-23 23:38:22 +0530

    Consult WebPage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import UpperNavbar from './Components/Navbar-Top/Navbar';
-import MainNavbar from './Components/Navbar/MainNavbar';
-import Employee_Profile from './Components/Employee-Image/Employee_Profile';
-import AboutConsult from './Components/AboutConsult/AboutConsult';
-import Offers from './Components/Offers/Offers';
-
-
+import logo from "./logo.svg";
+import "./App.css";
+// import UpperNavbar from './Components/Navbar-Top/Navbar';
+import MainNavbar from "./Components/Navbar/MainNavbar";
+import Employee_Profile from "./Components/Employee-Image/Employee_Profile";
+import AboutConsult from "./Components/AboutConsult/AboutConsult";
+import Offers from "./Components/Offers/Offers";
+import CompanyRating from "./Components/CompanyRating/CompanyRating";
 
 function App() {
   return (
     <>
-    <UpperNavbar/>
-    <MainNavbar/>
-    <Employee_Profile/>
-    <AboutConsult/>
-    <Offers/>
+      {/* <UpperNavbar/> */}
+      <MainNavbar />
+      <Employee_Profile />
+      <AboutConsult />
+      <Offers />
+      <CompanyRating/>
     </>
   );
 }
